2dacd901edf981ee307036ad852aa39e4c24e08c
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-01-06 14:58:03 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240106/코로나 메뉴얼/covid-manual.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char s1,s2,s3;
+    int temp1, temp2, temp3;
+
+    scanf("%c %d", &s1, &temp1);
+    getchar();
+    scanf("%c %d", &s2, &temp2);
+    getchar();
+    scanf("%c %d", &s3, &temp3);
+
+
+    int check = 0;
+
+    if(s1=='Y' && temp1 >=37){
+        check++;
+    }
+    if(s2=='Y' && temp2 >= 37){
+        check++;
+    }
+    if(s3=='Y' && temp3 >= 37){
+        check++;
+    }
+
+    if(check>=2){
+        printf("E");
+    }else{
+        printf("N");
+    }
+    return 0;
+}
