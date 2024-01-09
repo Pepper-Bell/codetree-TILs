7050e72861027243bcf372a162e0f341d172b524
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-01-09 12:54:56 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240109/n까지 숫자 출력/print-1-to-n.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int i=1;
+    scanf(" %d", &n);
+
+    while(i<n+1){
+        printf("%d ", i);
+        i++;
+    }
+    return 0;
+}
