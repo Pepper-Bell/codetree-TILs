e61d51fe92956d665024d0d8636281eb6d839095
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-01-10 23:42:48 +0900

    Title: a/b 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240110/a/b 출력/a-divide-b.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    int parent, child, left;
+    scanf("%d %d", &a, &b);
+
+    printf("%d.", a/b);
+
+    parent = a;
+    child = b;
+    left = parent;
+    
+    for(int i=0; i<20; i++){
+        parent = left*10;
+        printf("%d", parent/child);
+        left = parent%child;
+    }
+
+    return 0;
+}
