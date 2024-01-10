0756d4d648f2be3bb72719301d35ec9dcb8d0868
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-01-10 23:29:32 +0900

    Title: 짝수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/짝수만 출력/output-only-even.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    scanf("%d %d", &a, &b);
+
+    int i = a;
+
+    while(i<=b){
+        if(i%2==0){
+            printf("%d ", i);
+        }
+
+        i++;
+    }
+    return 0;
+}
