2dc7fbed72ee7ffcb689ce9b0200ccf8d9323864
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-01-11 20:37:46 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240111/369 게임/369-game.c
+++ 240111/369 게임/369-game.c
@@ -2,14 +2,24 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n;
+    int n; //0 to 100
     scanf(" %d", &n);
 
     for(int i=1; i<=n; i++){
-        if(i==3 || i==6 || i==9){
-            printf("0 ");
+        if(i>10){
+            if(i%10==3 || i%10==6 || i%10==9){
+                printf("0 ");
+            }else if(i/10==3 || i/10==6 || i/10==9){
+                printf("0 ");
+            }else{
+                printf("%d ", i);
+            }
         }else{
-            printf("%d ", i);
+            if(i==3 || i==6 || i==9){
+                printf("0 ");
+            }else{
+                printf("%d ", i);
+            }
         }
     }
     return 0;
