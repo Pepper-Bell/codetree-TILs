0b0f21a22171bee48e899e306efd698e9f98ca73
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-01-11 20:33:53 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240111/369 게임/369-game.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf(" %d", &n);
+
+    for(int i=1; i<=n; i++){
+        if(i==3 || i==6 || i==9){
+            printf("0 ");
+        }else{
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
