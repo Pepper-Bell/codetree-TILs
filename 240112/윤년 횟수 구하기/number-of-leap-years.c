f28725d552fe560dcd686e722595615dcb669d1b
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-01-12 20:18:14 +0900

    Title: 윤년 횟수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240112/윤년 횟수 구하기/number-of-leap-years.c
+++ 240112/윤년 횟수 구하기/number-of-leap-years.c
@@ -6,11 +6,11 @@ int main() {
     int cnt = 0;
     scanf(" %d", &n);
 
-    for(int i=0; i<=n; i++){
-        if(i%4==0){
-            if(i%100!=0 || i%400==0){
-                cnt++;
-            }
+    //2021
+
+    for(int i=1; i<=n; i++){
+        if(i%4==0 && i%100!=0 || i%400==0){
+            cnt++;
         }
     }
 
