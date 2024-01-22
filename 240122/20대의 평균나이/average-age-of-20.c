597e4de310a9094aace4c364d0e35694c3baedc0
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-01-22 10:33:06 +0900

    Title: 20대의 평균나이, Time: 1057ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240122/20대의 평균나이/average-age-of-20.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,cnt=0;
+    float sum=0;
+    while(1){
+        scanf(" %d", &n);
+        if(n<30){
+            cnt++;
+            sum+=n;
+        }else{
+            printf("%.2f", sum/cnt);
+            break;
+        }
+    }
+    return 0;
+}
