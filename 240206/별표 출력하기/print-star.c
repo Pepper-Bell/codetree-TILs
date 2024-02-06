cbc19c28cbf30adbda5b47c9da204bc1f91d0f75
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-02-06 11:51:41 +0900

    Title: 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240206/별표 출력하기/print-star.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    for(int i=1; i<n; i++){
+        for(int j=n-i; j>0; j--){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
