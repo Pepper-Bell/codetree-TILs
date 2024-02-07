252635b53088c2f9fb07c74bcb2c529e3eb36987
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-02-07 23:33:24 +0900

    Title: 체크 다이아몬드, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240207/체크 다이아몬드/check-diamond.c
+++ 240207/체크 다이아몬드/check-diamond.c
@@ -4,9 +4,9 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     scanf("%d", &n);
-
+    
     for(int i=0; i<n; i++){
-        for(int j=0; j<n-i; j++){
+        for(int j=1; j<n-i; j++){
             printf(" ");
         }
         for(int j=0; j<=i; j++){
@@ -16,7 +16,7 @@ int main() {
     }
 
     for(int i=1; i<n; i++){
-        for(int j=0; j<=i; j++){
+        for(int j=0; j<i; j++){
             printf(" ");
         }
         for(int j=0; j<n-i; j++){
