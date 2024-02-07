f2d32ce9df774ea8ac49d97344962a63fe5db5f0
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-02-08 00:01:00 +0900

    Title: 별표 출력하기 13, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240208/별표 출력하기 13/print-start-13.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+
+    int temp1=1, temp2=n;
+
+    for(int i=1; i<=n; i++){
+        if(i%2==0){
+            for(int j=0; j<temp1; j++){
+                printf("* ");
+            }
+            temp1++;
+        }else{
+            for(int j=0; j<temp2; j++){
+                printf("* ");
+            }
+            temp2--;
+        }
+        printf("\n");
+    }
+
+    temp1--;
+    temp2++;
+
+    if(n%2==0){
+        for(int i=1; i<=n; i++){
+            if(i%2==0){
+                for(int j=0; j<temp2; j++){
+                    printf("* ");
+                }
+                temp2++;
+            }else{
+                for(int j=0; j<temp1; j++){
+                    printf("* ");
+                }
+                temp1--;
+            }
+            printf("\n");
+        }
+    }else{
+        for(int i=1; i<=n; i++){
+            if(i%2==1){
+                for(int j=0; j<temp2; j++){
+                    printf("* ");
+                }
+                temp2++;
+            }else{
+                for(int j=0; j<temp1; j++){
+                    printf("* ");
+                }
+                temp1--;
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
