002ccba5616e57ad52439d4de91f41ad4a55fac2
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-07-01 02:18:39 +0900

    Title: 모양대로 숫자 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240701/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.c
+++ 240701/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.c
@@ -15,11 +15,11 @@ int main() {
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             printf("%d ", cnt);
-        }
-        if(cnt==8){
+            if(cnt==8){
             cnt = 2;
-        }else{
-            cnt += 2;
+            }else{
+                cnt += 2;
+            }
         }
         printf("\n");
     }
