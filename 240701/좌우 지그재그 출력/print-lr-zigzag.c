c63665c9627091e2a07e45dfbbaa125179369a08
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-07-01 20:18:58 +0900

    Title: 좌우 지그재그 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240701/좌우 지그재그 출력/print-lr-zigzag.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+
+    //meet the condition
+    if(n<1 || n>100){
+        return 0;
+    }
+    //meet the condition
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i%2==0){
+                printf("%d ", (i*n)+j+1);
+            }else{
+                printf("%d ", (i*n)+n-j);
+            }
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
