02c3af10297c9df90c26c79afe1ddd6ced480a0d
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-07-02 17:41:39 +0900

    Title: 3n + 1 수열 m번, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240702/3n + 1 수열 m번/3n-plus-1-sequence-m-times.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m, cnt=0;
+    scanf("%d ", &m);
+
+    for(int i=0; i<m; i++){
+        scanf("%d ", &n);
+        while(n!=1){
+            if(n%2==0){
+                n /= 2;
+                cnt++;
+            }else{
+                n *= 3;
+                n++;
+                cnt++;
+            }
+        }
+
+        printf("%d", cnt);
+    }
+    return 0;
+}
