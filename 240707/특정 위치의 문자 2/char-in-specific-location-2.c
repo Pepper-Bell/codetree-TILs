b6bb828ad57b6d8a65f243a58a88800984ca3ed9
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-07-07 16:33:08 +0900

    Title: 특정 위치의 문자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240707/특정 위치의 문자 2/char-in-specific-location-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char arr[10];
+
+    for(int i=0; i<10; i++){
+        scanf("%c ", &arr[i]);
+        if(i==1 || i==4 || i==7){
+            printf("%c ", arr[i]);
+        }
+    }
+
+
+    return 0;
+}
