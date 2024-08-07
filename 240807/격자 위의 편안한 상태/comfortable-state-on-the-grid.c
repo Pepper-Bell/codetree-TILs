cf68cfbfefb28e9ab010ce2f982a51c611b18312
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-08-07 22:26:24 +0900

    Title: 격자 위의 편안한 상태, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240807/격자 위의 편안한 상태/comfortable-state-on-the-grid.c
+++ 240807/격자 위의 편안한 상태/comfortable-state-on-the-grid.c
@@ -1,54 +1,36 @@
 #include <stdio.h>
 
+int check(int a, int b);
+static int arr[102][102]={0, };
+
 int main() {
 
-    int n,m,a,b,check=0;
+    int n,m,a,b;
     scanf("%d %d", &n, &m);
-
-    int arr[n][n];
-
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            arr[i][j]=0;
-        }
-    }
 
     for(int i=0; i<m; i++){
         scanf("%d %d", &a, &b);
-        arr[a-1][b-1] = 1;
-
-        if(arr[a-2][b-1]==1){
-            if(a-2 < 0){
-                continue;
-            }else{
-                check++;
-            }}//up
-        if(arr[a][b-1]==1){
-            if(a > n){
-                continue;
-            }else{
-                check++;
-            }}//down
-        if(arr[a-1][b-2]==1){
-            if(b-2 < 0){
-                continue;
-            }else{
-                check++;
-            }}//left
-        if(arr[a-1][b]==1){
-            if(b > n){
-                continue;
-            }else{
-                check++;
-            }}//right
-
-        if(check>=3){
-            printf("1\n");
-        }else{
-            printf("0\n");
-        }
-        check = 0;
+        printf("%d\n", check(a-1, b-1));
     }
 
     return 0;
+}
+
+int check(int a, int b){
+    //up, down, left, right
+    int sat = 0;
+    int da[4]={-1, 1, 0, 0};
+    int db[4]={0, 0, -1, 1};
+    arr[a][b] = 1;
+    for(int i=0; i<4; i++){
+        if(arr[a+da[i]][b+db[i]]==1){
+            sat++;
+        }
+    }
+    
+    if(sat>=3){
+        return 1;
+    }else{
+        return 0;
+    }
 }
