0d7fb08a3ba2c5ebd37cdcafe5df6eacef2d6cf4
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-08-07 22:39:43 +0900

    Title: 격자 위의 편안한 상태, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240807/격자 위의 편안한 상태/comfortable-state-on-the-grid.c
+++ 240807/격자 위의 편안한 상태/comfortable-state-on-the-grid.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int check(int a, int b);
+bool inrange(int a, int b);
+
 static int arr[102][102]={0, };
+static int n,m;
 
 int main() {
 
-    int n,m,a,b;
+    int a,b;
     scanf("%d %d", &n, &m);
 
     for(int i=0; i<m; i++){
@@ -16,6 +20,10 @@ int main() {
     return 0;
 }
 
+bool inrange(int a, int b){
+    return 0 <= a && a < n && 0 <= b && b < n;
+}
+
 int check(int a, int b){
     //up, down, left, right
     int sat = 0;
@@ -23,7 +31,7 @@ int check(int a, int b){
     int db[4]={0, 0, -1, 1};
     arr[a][b] = 1;
     for(int i=0; i<4; i++){
-        if(arr[a+da[i]][b+db[i]]==1){
+        if(inrange(a+da[i],b+db[i]) && arr[a+da[i]][b+db[i]]==1){
             sat++;
         }
     }
