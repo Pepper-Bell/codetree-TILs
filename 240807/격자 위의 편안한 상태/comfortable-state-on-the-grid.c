6e4f455867e42e24c2c7205533fd2eed3d87f83a
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-08-07 22:16:14 +0900

    Title: 격자 위의 편안한 상태, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240807/격자 위의 편안한 상태/comfortable-state-on-the-grid.c
+++ 240807/격자 위의 편안한 상태/comfortable-state-on-the-grid.c
@@ -5,25 +5,42 @@ int main() {
     int n,m,a,b,check=0;
     scanf("%d %d", &n, &m);
 
-    //meet the condition
-    if(n>100 || n<1 || m<1 || m>n*n){
-        return 0;
-    }
-
     int arr[n][n];
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            arr[i][j]=0;
+        }
+    }
 
     for(int i=0; i<m; i++){
         scanf("%d %d", &a, &b);
-        //condition
-        if(a<1 || b<1 || a>n || b>n){
-            return 0;
-        }
         arr[a-1][b-1] = 1;
 
-        if(arr[a-2][b-1]==1){check++;}//up
-        if(arr[a][b-1]==1){check++;}//down
-        if(arr[a-1][b-2]==1){check++;}//left
-        if(arr[a-1][b]==1){check++;}//right
+        if(arr[a-2][b-1]==1){
+            if(a-2 < 0){
+                continue;
+            }else{
+                check++;
+            }}//up
+        if(arr[a][b-1]==1){
+            if(a > n){
+                continue;
+            }else{
+                check++;
+            }}//down
+        if(arr[a-1][b-2]==1){
+            if(b-2 < 0){
+                continue;
+            }else{
+                check++;
+            }}//left
+        if(arr[a-1][b]==1){
+            if(b > n){
+                continue;
+            }else{
+                check++;
+            }}//right
 
         if(check>=3){
             printf("1\n");
