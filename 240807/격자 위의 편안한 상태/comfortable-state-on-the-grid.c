fba7b5ae03c60088437a52a5f3c636fbd58dbc45
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-08-07 21:55:51 +0900

    Title: 격자 위의 편안한 상태, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240807/격자 위의 편안한 상태/comfortable-state-on-the-grid.c
+++ 240807/격자 위의 편안한 상태/comfortable-state-on-the-grid.c
@@ -5,10 +5,19 @@ int main() {
     int n,m,a,b,check=0;
     scanf("%d %d", &n, &m);
 
+    //meet the condition
+    if(n>100 || n<1 || m<1 || m>n*n){
+        return 0;
+    }
+
     int arr[n][n];
 
     for(int i=0; i<m; i++){
         scanf("%d %d", &a, &b);
+        //condition
+        if(a<1 || b<1 || a>n || b>n){
+            return 0;
+        }
         arr[a-1][b-1] = 1;
 
         if(arr[a-2][b-1]==1){check++;}//up
