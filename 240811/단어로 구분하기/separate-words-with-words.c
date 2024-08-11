7c827b65d48fb49604650dcf8a7bc1f1225ff8c3
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-08-11 15:32:15 +0900

    Title: 단어로 구분하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240811/단어로 구분하기/separate-words-with-words.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char str[10][20];
+    for(int i=0; i<10; i++){
+        scanf("%s", &str[i]);
+    }
+    for(int i=0; i<10; i++){
+        printf("%s\n", str[i]);
+    }
+    return 0;
+}
