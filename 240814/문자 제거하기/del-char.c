86952db15ae89a39da8d278870d11ea84dd3c614
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-08-14 20:30:59 +0900

    Title: 문자 제거하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240814/문자 제거하기/del-char.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char arr[21];
+    scanf("%s", arr);
+    int len = strlen(arr);
+    int n;
+
+    while(len != 1){
+        scanf("%d", &n);
+        if(n>len){
+            arr[len-1] = '\0';
+        }else{
+            for(int i=n; i<len; i++){
+                arr[i] = arr[i+1];
+                if(i==len-1){
+                    arr[i] = '\0';
+                }
+            }
+        }
+        len--;
+        printf("%s\n", arr);
+    }
+
+    return 0;
+}
