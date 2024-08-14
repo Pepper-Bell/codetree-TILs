3cad02e162fbd44461d5294e8f414f322a456ba4
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-08-14 21:11:40 +0900

    Title: 문자열 계속 지우기, Time: 3ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240814/문자열 계속 지우기/keep-removing-string.c
+++ 240814/문자열 계속 지우기/keep-removing-string.c
@@ -3,7 +3,7 @@
 
 int main() {
     char arr1[101], arr2[101];
-    int check = 0, len;
+    int check = 0, len, cnt=0;
     scanf("%s", arr1);
     scanf("%s", arr2);
 
@@ -12,12 +12,21 @@ int main() {
     while(check == 0){
         check = 1;
         for(int i=0; i<len; i++){
-            if(arr1[i]==arr2[0] && arr1[i+1]==arr2[1]){
-                check = 0;
-                for(int j=i; j<len-2; j++){
-                    arr1[j] = arr1[j+2];
-                    if(j==len-3){
-                        arr1[j+1] = '\0';
+            if(arr1[i]==arr2[0]){
+                for(int j=0; j<strlen(arr2); j++){
+                    if(arr1[i+j]==arr2[j]){
+                        cnt++;
+                    }else{
+                        cnt = 0;
+                    }
+                }
+                if(cnt==strlen(arr2)){
+                    check = 0;
+                    for(int k=i; k<len-strlen(arr2); k++){
+                        arr1[k] = arr1[k+strlen(arr2)];
+                        if(k==len-strlen(arr2)-1){
+                            arr1[k+1]='\0';
+                        }
                     }
                 }
             }
