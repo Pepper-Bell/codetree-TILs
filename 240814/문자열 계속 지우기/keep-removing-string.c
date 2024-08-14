94bf386a5ea751061370f2bc183e817df4cfcd83
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-08-14 21:32:08 +0900

    Title: 문자열 계속 지우기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240814/문자열 계속 지우기/keep-removing-string.c
+++ 240814/문자열 계속 지우기/keep-removing-string.c
@@ -3,14 +3,13 @@
 
 int main() {
     char arr1[101], arr2[101];
-    int check = 0, len, cnt=0;
+    int check = 0, len, cnt=0, lastcnt=0;
     scanf("%s", arr1);
     scanf("%s", arr2);
 
-    len = strlen(arr1);
-
     while(check == 0){
         check = 1;
+        len = strlen(arr1);
         for(int i=0; i<len; i++){
             if(arr1[i]==arr2[0]){
                 for(int j=0; j<strlen(arr2); j++){
@@ -33,6 +32,14 @@ int main() {
         }
     }
 
-    printf("%s", arr1);
+    for(int i=0; i<len; i++){
+        if(arr1[i]==arr2[i]){
+            lastcnt++;
+        }
+    }
+
+    if(lastcnt!=len) {
+        printf("%s", arr1);
+    }
     return 0;
 }
