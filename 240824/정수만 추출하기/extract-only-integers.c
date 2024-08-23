b0c57b9b034db6f1310e877681eef873f37960a3
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-08-24 00:03:10 +0900

    Title: 정수만 추출하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240824/정수만 추출하기/extract-only-integers.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+    char str1[10], str2[10];
+    char change1[10], change2[10];
+
+    scanf("%s %s", str1, str2);
+
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+    int cnt=0;
+
+    for(int i=0; i<len1; i++){
+        if(str1[i]>='0'&&str1[i]<='9'){
+            change1[cnt]=str1[i];
+            cnt++;
+        }else{
+            change1[cnt]='\0';
+            break;
+        }
+    }
+
+    cnt = 0;
+
+    for(int i=0; i<len2; i++){
+        if(str2[i]>='0'&&str2[i]<='9'){
+            change2[cnt]=str2[i];
+            cnt++;
+        }else{
+            change2[cnt]='\0';
+            break;
+        }
+    }
+
+    printf("%d", atoi(change1)+atoi(change2));
+
+    return 0;
+}
