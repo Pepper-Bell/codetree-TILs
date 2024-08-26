029c8ccb1a9a5049ab4dabea1e2ead3de9d025c2
chohyeon <106971788+Pepper-Bell@users.noreply.github.com>
2024-08-26 23:22:48 +0900

    Title: 문자열 계속 지우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240826/문자열 계속 지우기/keep-removing-string.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char A[101]="", B[101]="";
+    int cnt = 0;
+
+    scanf("%s %s", A, B);
+
+    int alen = strlen(A);
+    int blen = strlen(B);
+
+    for(int i=0; i<alen; i++){
+        for(int j=0; j<blen; j++){
+            if(A[i+j]==B[j]){
+                cnt++;
+            }else{
+                cnt=0;
+                break;
+            }
+            if(cnt==blen){
+                for(int k=i; k<alen-blen; k++){
+                    A[k] = A[k+blen];
+                }
+                A[alen-blen]='\0';
+                alen = strlen(A);
+                cnt=0;
+                i=0;
+            }
+        }
+    }
+
+    if(strcmp(A, B)==0){
+    }else{
+        printf("%s", A);
+    }
+
+    return 0;
+}
